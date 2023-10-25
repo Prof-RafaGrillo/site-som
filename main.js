ee5c9b13933bb37939f452798535664952bc81ef
Prof-RafaGrillo <125394387+Prof-RafaGrillo@users.noreply.github.com>
2023-10-25 11:58:50 -0300

    .

--- main.js
+++ main.js
@@ -3,4 +3,11 @@ function tocaSomPom(){
     document.querySelector('#som_tecla_pom').play()
 
 }
+
+function tocaSomClap(){
+
+    document.querySelector('#som_tecla_clap').play()
+
+}
 document.querySelector('.tecla_lightsaber').onclick = tocaSomPom
+document.querySelector('.tecla_breath').onclick = tocaSomClap
