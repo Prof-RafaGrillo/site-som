bb0200902cc74ad7f2488f82596e70458b3009ce
Prof-RafaGrillo <125394387+Prof-RafaGrillo@users.noreply.github.com>
2023-11-01 11:53:08 -0300

    .

--- main.js
+++ main.js
@@ -4,9 +4,10 @@ function tocaSomPom(){
 
 const listaDeTeclas = document.querySelectorAll('.tecla')
 
-listaDeTeclas[0].onclick = tocaSomPom
-listaDeTeclas[1].onclick = tocaSomPom
-listaDeTeclas[2].onclick = tocaSomPom
-listaDeTeclas[3].onclick = tocaSomPom
-listaDeTeclas[4].onclick = tocaSomPom
-listaDeTeclas[5].onclick = tocaSomPom
+let contador = 0;
+
+while (contador < 9 ){
+    listaDeTecla[contador].onclick = tocaSomPom;
+    contador = contador + 1;
+    }
+    
