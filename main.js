c3579cf2d5df9a2156c032fb8bde9b570c173401
Prof-RafaGrillo <125394387+Prof-RafaGrillo@users.noreply.github.com>
2023-11-22 11:33:30 -0300

    .

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play()
+document.querySelector('#som_tecla_clap').play()
+
+function tocaSom(idElemento){
+    document.querySelector(idElemento).play()
 }
 alert("teste")
 const listaDeTeclas = document.querySelectorAll('.tecla')
@@ -7,7 +9,7 @@ const listaDeTeclas = document.querySelectorAll('.tecla')
 let contador = 0;
 
 while (contador < listaDeTeclas.length ){
-    listaDeTeclas[contador].onclick = tocaSomPom;
+    listaDeTeclas[contador].onclick = tocaSom('#som_tecla_pom');
     contador = contador + 1;
     }
     
