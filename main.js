d1ba4bd03b7eae0a4dc8c9e24e5fd9b2b14703a8
Prof-RafaGrillo <125394387+Prof-RafaGrillo@users.noreply.github.com>
2023-11-22 11:58:12 -0300

    .

--- main.js
+++ main.js
@@ -9,6 +9,8 @@ const listaDeTeclas = document.querySelectorAll('.tecla')
 let contador = 0;
 
 while (contador < listaDeTeclas.length ){
+    const instrumento = listaDeTeclas[contador].classList
+    console.log(instrumento)
     listaDeTeclas[contador].onclick = function (){
 
         tocaSom('#som_tecla_pom');
